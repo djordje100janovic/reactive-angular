a194e78140e20bb9ae8dd4ebb3e906e9c8d738f4
Djordje Stojanovic <djordje.stojanovic@htecgroup.com>
2022-11-02 14:17:48 +0100

    updated styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  busy: boolean = false;
+  error: boolean = false;
+  fetchSubject = new Subject<boolean>();
+  fetch$ = this.fetchSubject.asObservable();
 
-  ngOnInit(): void {
+  categories$ = this.fetch$.pipe(
+    tap(() => this.busy = true),
+    switchMap((res) => this.http.get<any>('assets/categories.json')),
+    tap(() => this.busy = false)
+  );
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  fetch(): void {
+    this.fetchSubject.next(true);
   }
 
 }
